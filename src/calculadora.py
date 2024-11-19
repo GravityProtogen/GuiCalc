36e495d569a0de5243cf4a19fb086f02db04d6c9
Gravity Proto <pedroarturcosta@hotmail.com>
2024-11-19 12:01:06 -0300

    Input e sizing

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,20 +1,17 @@
 import sys
 
 from mainwindow import MainWindow
-from PySide6.QtWidgets import QApplication, QLabel
-from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from variables import WINDOW_ICON_PATH
+from display import Display
 
 if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
 
-    label1 = QLabel('O meu texto')
-    label1.setStyleSheet('font-size: 150px;')
-    window.v_layout.addWidget(label1)
-    window.adjustFixedSize()
+
     # Define o ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
@@ -25,6 +22,11 @@ if __name__ == '__main__':
         import ctypes
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
             u'CompanyName.ProductName.SubProduct.VersionInformation')
+
+    # Display
+    display = Display()
+    window.addToVLayout(display)
+
 
     # Executa tudo
     window.adjustFixedSize()
--- src/variables.py
+++ src/variables.py
@@ -3,3 +3,10 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent.resolve()
 
 WINDOW_ICON_PATH = ROOT_DIR / 'icon.png'
+
+#Sizing
+BIG_FONT_SIZE = 40
+MEDIUM_FONT_SIZE = 24
+SMALL_FONT_SIZE = 18
+TEXT_MARGIN = 15
+MINIMUN_WIDTH = 300
