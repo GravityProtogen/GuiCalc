1e6b6f59d2a27acae9d9412640bd8cdd1d331650
Gravity Proto <pedroarturcosta@hotmail.com>
2024-11-22 09:27:05 -0300

    Dark theme

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,14 +1,17 @@
 import sys
-
-from mainwindow import MainWindow
-from PySide6.QtWidgets import QApplication
-from PySide6.QtGui import QIcon
-from variables import WINDOW_ICON_PATH
+ 
 from display import Display
+from info import Info
+from mainwindow import MainWindow
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+from styles import setupTheme
+from variables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
+    setupTheme(app)
     window = MainWindow()
 
 
@@ -16,6 +19,10 @@ if __name__ == '__main__':
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+    
+    # Info
+    info = Info('242.3 ^ 12.45' )
+    window.addToVLayout(info)    
     
     # Configuração para o icon aparecer na taskbar do Windows
     if sys.platform.startswith('win'):
